4490706de8725c5d75710e84a161971ae56fe75f
kevanchew96 <73065966+kevanchew96@users.noreply.github.com>
2020-11-05 18:44:15 +0800

    Update app(edit this one).R

--- app(edit this one).R
+++ app(edit this one).R
@@ -28,6 +28,8 @@ set_key(key = key)
 #DATA SOURCE & CLEANING ################################################################################################################################################################
 
 final_out <- read.csv("Resale_Region.csv")
+resale_avail <- read.csv("Resale_Avail.csv")
+
 
 bto <- read.csv("BTOPredict.csv")
 bto[6,1] <- 6
