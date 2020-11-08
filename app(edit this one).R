9b69aad7cc47874c111bcc74d122c8a6fa01faef
kjinjie <61398903+kjinjie@users.noreply.github.com>
2020-11-08 14:33:59 +0800

    Update app(edit this one).R

--- app(edit this one).R
+++ app(edit this one).R
@@ -346,7 +346,7 @@ resale_breakdown<- function(base_price, income, flat_type, distance_from_p, with
     ylab("Amount (SG$)") + xlab("") + 
     scale_fill_discrete(name = "Type") +
     scale_fill_manual("legend", values = c("Balance" = "#999999", "Downpayment" = "#E69F00", "Family Grant" = "#3F704D", "Singles Grant" = "#4F7942", "Half housing Grant" = "#00A86B", "Enhanced Housing Grant" = "#708238", "Singles Enhanced Housing Grant" = "#98FB98", "Proximity Grant" = "#D0F0C0")) +
-    scale_y_continuous(label=comma) +
+    scale_y_continuous(label=comma,limits= c(0,650000)) +
     theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank())
@@ -399,14 +399,14 @@ bto_breakdown<- function(base_price, income, is_married, citizenship, applicatio
   df[,2] <- types
   df[,3] <- bto_cost_breakdown
   
-
+  
   breakdown_plot <- ggplot(df, aes(fill=type, y=amount, x=total_price)) + 
     geom_bar(position=position_stack(reverse = TRUE), stat="identity") + 
-    geom_text(aes(label = stat(y), group = total_price), stat = 'summary', fun=sum) +  #ADD THE TOTAL SUM ABOVE
+    geom_text(aes(label = stat(y), group = total_price), stat = 'summary', fun=sum, position=position_dodge(width=0.9)) +  #ADD THE TOTAL SUM ABOVE
     ylab("Amount (SG$)") + xlab("") + 
     scale_fill_discrete(name = "Type") + 
-    scale_y_continuous(label=comma) +
     scale_fill_manual("legend", values = c("Balance" = "#999999", "Downpayment" = "#E69F00", "Enhanced Housing Grant" = "#9DC183", "Singles Enhanced Housing Grant" = "#708238")) +
+    scale_y_continuous(label=comma,limits= c(0,650000)) +
     theme(axis.title.x=element_blank(),
           axis.text.x=element_blank(),
           axis.ticks.x=element_blank())
